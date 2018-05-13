4e0bffd2cd4177b26e9085cf920fdad6795f8f3b
“Buratinka” <oleg.shved.shvets@gmail.com>
2018-05-14 01:21:01 +0300

    19lvl

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -0,0 +1,169 @@
+package com.javarush.task.task19.task1918;
+
+
+
+
+///////////////////////////////////////////////////////// my solution
+
+/* 
+Знакомство с тегами
+*/
+/*
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Solution {
+
+
+    private static ArrayList<String> allTags = new ArrayList<>();
+
+    private static String helper = "";
+    private static String helper2 = "";
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader fileName = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader fileReader = new BufferedReader(new FileReader((fileName.readLine())));
+
+
+
+
+        while(fileReader.ready())
+        {
+            String str = fileReader.readLine();
+
+
+            for(int i =0;i<4;i++) {
+
+                try {
+
+                    helper = str.substring(str.indexOf("<"+args[0]) + 6, str.indexOf("</"+args[0]+">") + 7);
+
+
+                    if (helper.contains("<"+args[0])) {
+
+
+                        helper2 = str.substring(str.indexOf("<"+args[0]),str.indexOf("</b></"+args[0]+">")+11);
+
+                        allTags.add(helper2);
+
+                        helper2 = helper.substring(helper.indexOf("<"+args[0]), helper.length());
+
+                        allTags.add(helper2);
+
+
+
+                        str = str.replace(helper2, "");
+
+
+
+                    }
+                    else {
+                        if(!helper.contains("xml"))
+                        {
+
+                            helper = "<"+args[0]+">"+helper;
+
+                            allTags.add(helper);
+
+                        }
+                        else
+                        {
+
+                            helper = "<"+args[0]+" "+helper;
+
+
+                        }
+
+                        str = str.replace(helper,"");
+
+                    }
+
+
+                }
+
+                catch (StringIndexOutOfBoundsException e)
+                { }
+
+
+            }
+        }
+
+        for(String s : allTags)
+        {
+            System.out.println(s);
+        }
+
+
+        fileName.close();
+        fileReader.close();
+    }
+}
+*/
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader conReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = conReader.readLine();
+        conReader.close();
+
+        //Read File
+        BufferedReader fileBufReader = new BufferedReader(new FileReader(fileName));
+        StringBuffer content = new StringBuffer();
+        while (fileBufReader.ready())
+            content.append(fileBufReader.readLine());
+        fileBufReader.close();
+        StringBuffer text = new StringBuffer(content.toString().replaceAll("\r\n", ""));
+
+        String tagOpen = "<" + args[0];
+        String tagClose = "</" + args[0] + ">";
+
+        int pozitionOpen = -1;
+        int pozitionClose = -1;
+        int shift = -1;
+        Stack<Integer> openedTags = new Stack<>();
+        Map<Integer, Integer> tags = new TreeMap<>(new MyComparator());
+        while (true) {
+            pozitionOpen = text.indexOf(tagOpen, shift);
+            pozitionClose = text.indexOf(tagClose, shift);
+            if (pozitionOpen < 0 && pozitionClose < 0)
+                break;
+
+            if (pozitionOpen != -1 && pozitionOpen < pozitionClose) { //Open ближе чем close
+                openedTags.push(pozitionOpen);
+                shift = pozitionOpen + tagOpen.length();
+                continue;
+            }
+
+            if (pozitionClose != -1 && (pozitionOpen > pozitionClose || pozitionOpen == -1)) { //Close ближе чем open
+                if (openedTags.isEmpty())
+                    break;
+                tags.put(openedTags.pop(), pozitionClose + tagClose.length());
+                shift = pozitionClose + tagClose.length();
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> pair : tags.entrySet()) {
+            System.out.println(text.substring(pair.getKey(), pair.getValue()));
+        }
+    }
+
+    static class MyComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer a, Integer b) {
+            return a.compareTo(b);
+        }
+    }
+}
